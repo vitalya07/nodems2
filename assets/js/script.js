e632c14b4c29fe77665881ead23e3ea86b444320
Vitalyi <saifutdinov_vitalii@mail.ru>
2025-11-28 00:27:12 +0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    // Мобильное меню начало
+    const header = document.querySelector('.header');
+    const hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', ()=> {
+        header.classList.toggle('menu__active');
+    });
+    // Мобильное меню конец
+})
